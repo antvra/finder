d17fa3f9690348347ff056a97e02ba4ff1c2b577
Swiftrade <swiftradeera@gmail.com>
2022-01-09 14:12:49 +0300

    eventListeners fix

--- js/app.js
+++ js/app.js
@@ -14,13 +14,32 @@ const createSearchBlock = (repoName) => {
 
 const createAddedBlock = ([name, owner, stars] = info) => {
   const block = document.querySelector(".block__elements-result");
-  const blockWithResult = `<div class = 'block__added'><div class = 'block__added--info'><h4 class = 'block__added--text'>Name: ${name}</h4><h4 class = 'block__added--text'>Owner: ${owner}</h4><h4 class = 'block__added--text'>Stars: ${stars}</h4></div><button class = 'block__added--delete'>X</button></div>`;
-  block.innerHTML = blockWithResult;
-  const btn = document.querySelector(".block__added--delete");
-  function removeBlock() {
-    block.innerHTML = "";
+  const blockWithResult = document.createElement("div");
+  blockWithResult.classList.add("block__added");
+  const infoBlock = document.createElement("div");
+  infoBlock.classList.add("block__added--info");
+  const nameInfo = document.createElement("h4");
+  const ownerInfo = document.createElement("h4");
+  const starsInfo = document.createElement("h4");
+  nameInfo.classList.add("block__added--text");
+  ownerInfo.classList.add("block__added--text");
+  starsInfo.classList.add("block__added--text");
+  nameInfo.textContent = `Name: ${name}`;
+  ownerInfo.textContent = `Owner: ${owner}`;
+  starsInfo.textContent = `Stars: ${stars}`;
+  const btn = document.createElement("button");
+  btn.classList.add("block__added--delete");
+  btn.textContent = "X";
+  infoBlock.append(nameInfo);
+  infoBlock.append(ownerInfo);
+  infoBlock.append(starsInfo);
+  blockWithResult.append(infoBlock);
+  blockWithResult.append(btn);
+  block.append(blockWithResult);
+  const removeBlock = () => {
     btn.removeEventListener("click", removeBlock);
-  }
+    blockWithResult.remove();
+  };
   btn.addEventListener("click", removeBlock);
 };
 
@@ -41,6 +60,7 @@ const getRepos = async (repoName) => {
 
 const getListFromInput = async () => {
   const results = await getRepos(input.value);
+  inputResults.addEventListener("click", clickOnList);
   if (option == null) {
     removeList();
   }
@@ -64,13 +84,14 @@ const debounce = (fn, debounceTime) => {
   };
 };
 
-input.addEventListener("input", debounce(getListFromInput, 500));
-
-inputResults.addEventListener("click", (event) => {
+const clickOnList = (event) => {
   let target = event.target;
   target.classList.add("block__option--active");
   const name = target.textContent;
   createAddedBlock(list.get(name));
   input.value = "";
   removeList();
-});
+  inputResults.removeEventListener("click", clickOnList);
+};
+
+input.addEventListener("input", debounce(getListFromInput, 500));
